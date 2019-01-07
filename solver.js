eef6f3d2df0f8a81bf927fea7a7bab01f001a345
Matthew Meinzer <meinzer.matthew@gmail.com>
2019-01-06 22:35:05 -0600

    Python calls Javascript solver

---
+++ solver.js
@@ -0,0 +1,16 @@
+if (!process.argv[2]) {
+  throw new Error("Expected input data");
+}
+const { numberOfItems, size, items } = parseInputData(process.argv[2]);
+
+function parseInputData(inputString) {
+  const lines = inputString.split("\n").filter(str => str !== "");
+  const [numberOfItems, size] = lines[0]
+    .split(" ")
+    .map(str => parseInt(str, 10));
+  const items = lines.slice(1).map(itemString => {
+    const [value, weight] = itemString.split(" ").map(str => parseInt(str, 10));
+    return { value, weight };
+  });
+  return { numberOfItems, size, items };
+}
--- solver.py
+++ solver.py
@@ -1,44 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 from collections import namedtuple
+from subprocess import call
+
 Item = namedtuple("Item", ['index', 'value', 'weight'])
 
 def solve_it(input_data):
-    # Modify this code to run your optimization algorithm
+    res = call(["node","./solver.js",input_data])
 
-    # parse the input
-    lines = input_data.split('\n')
-
-    firstLine = lines[0].split()
-    item_count = int(firstLine[0])
-    capacity = int(firstLine[1])
-
-    items = []
-
-    for i in range(1, item_count+1):
-        line = lines[i]
-        parts = line.split()
-        items.append(Item(i-1, int(parts[0]), int(parts[1])))
-
-    # a trivial greedy algorithm for filling the knapsack
-    # it takes items in-order until the knapsack is full
-    print(items)
-    value = 0
-    weight = 0
-    taken = [0]*len(items)
-
-    for item in items:
-        if weight + item.weight <= capacity:
-            taken[item.index] = 1
-            value += item.value
-            weight += item.weight
-    
-    # prepare the solution in the specified output format
-    output_data = str(value) + ' ' + str(0) + '\n'
-    output_data += ' '.join(map(str, taken))
+    output_data = "From Python output: fix me"
     return output_data
-
 
 if __name__ == '__main__':
     import sys
