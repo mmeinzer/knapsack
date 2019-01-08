8693adb6b983dc6a496e2c133995debd10b546a2
Matthew Meinzer <meinzer.matthew@gmail.com>
2019-01-07 19:57:04 -0600

    Add theoretical maximum using linear relaxation

--- solver.js
+++ solver.js
@@ -5,6 +5,8 @@ if (!process.argv[2]) {
 }
 const data = parseInputData(process.argv[2]);
 
+theoreticalMaxValue(data);
+
 const algos = [valueDensityGreedy];
 algos.forEach(algo => {
   const { value, solution } = algo(data);
@@ -12,31 +14,62 @@ algos.forEach(algo => {
   fs.writeFileSync("./answer", string);
 });
 
+function addItemDetails(item, indexToSave) {
+  return {
+    ...item,
+    originalIndex: indexToSave,
+    valueDensity: item.value / item.weight
+  };
+}
+
 function valueDensityGreedy(data) {
   const { capacity, items } = data;
   const sortedItems = items
-    .map((item, i) => {
-      item.valueDensity = item.value / item.weight;
-      item.originalIndex = i;
-      return item;
-    })
+    .map(addItemDetails)
     .sort((a, b) => b.valueDensity - a.valueDensity);
 
   let remainingCapacity = capacity;
   let value = 0;
   const solution = sortedItems
     .map(item => {
-      if (item.weight <= remainingCapacity) {
-        value += item.value;
-        remainingCapacity -= item.weight;
-        item.used = 1;
+      const itemCopy = { ...item };
+      if (itemCopy.weight <= remainingCapacity) {
+        value += itemCopy.value;
+        remainingCapacity -= itemCopy.weight;
+        itemCopy.used = 1;
       } else {
-        item.used = 0;
+        itemCopy.used = 0;
       }
-      return item;
+      return itemCopy;
     })
     .sort((a, b) => a.originalIndex - b.originalIndex);
   return { solution, value };
+}
+
+function theoreticalMaxValue(data) {
+  const { capacity, items } = data;
+  // Determines a theoretical maximum for the knapsack problem using linear relaxation
+  // i.e. partial items are allowed
+  const sortedItems = items
+    .map(addItemDetails)
+    .sort((a, b) => b.valueDensity - a.valueDensity);
+  let remainingCapacity = capacity;
+  let maxValue = 0;
+  sortedItems.some((item, i) => {
+    // Array.prototype.some will continue execution as long as false is returned
+    // ...but short circuit on true
+    if (item.weight < remainingCapacity) {
+      maxValue += item.value;
+      remainingCapacity -= item.weight;
+      return false;
+    } else {
+      const fractionOfItem = remainingCapacity / item.weight;
+      maxValue += fractionOfItem * item.value;
+      remainingCapacity -= fractionOfItem * item.weight;
+      return true;
+    }
+  });
+  return maxValue;
 }
 
 function parseInputData(inputString) {
